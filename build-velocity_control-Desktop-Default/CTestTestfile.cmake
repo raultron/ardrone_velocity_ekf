35464b31098dce7a0a5064435d0b6a7899bfed8b
Tobias Tueylue <tobias.tuylu@gmail.com>
2016-08-15 10:44:05 +0200

    Final

--- build-velocity_control-Desktop-Default/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tobias/tum_ws/src/ardrone_velocity/velocity_control
-# Build directory: /home/tobias/tum_ws/src/ardrone_velocity/build-velocity_control-Desktop-Default
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
--- build-velocity_control-Desktop-Default/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/usr/local/include".split(';') if "/usr/local/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "dynamic_reconfigure;geometry_msgs;nav_msgs;roscpp;rospy;std_msgs;tf2".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "velocity_control"
-PROJECT_SPACE_DIR = "/usr/local"
-PROJECT_VERSION = "0.0.0"
